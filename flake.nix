56df853ac39e5f4a98420db88a26d5f9cf93be3a
qazer <114782572+qazer2687@users.noreply.github.com>
2025-06-04 02:06:40 +0100

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Thorium using Nix Flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: {
+    ##### aarch64-linux #####
+    packages.aarch64-linux = {
+      thorium = let
+        pkgs = import nixpkgs {system = "aarch64-linux";};
+        name = "thorium";
+        version = "130.0.6723.174";
+        src = pkgs.fetchurl {
+          url = "https://github.com/Alex313031/Thorium-Raspi/releases/download/M130.0.6723.174/Thorium_Browser_130.0.6723.174_arm64.AppImage";
+          sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+        };
+        appimageContents = pkgs.appimageTools.extractType2 {inherit name src;};
+      in
+        pkgs.appimageTools.wrapType2 {
+          inherit name version src;
+          extraInstallCommands = ''
+            install -m 444 -D ${appimageContents}/thorium-browser.desktop $out/share/applications/thorium-browser.desktop
+            install -m 444 -D ${appimageContents}/thorium.png $out/share/icons/hicolor/512x512/apps/thorium.png
+            substituteInPlace $out/share/applications/thorium-browser.desktop \
+            --replace 'Exec=AppRun --no-sandbox %U' 'Exec=${name} %U'
+          '';
+        };
+
+      default = self.packages.aarch64-linux.thorium;
+    };
+
+    apps.aarch64-linux = {
+      thorium = {
+        type = "app";
+        program = "${self.packages.aarch64-linux.thorium}/bin/thorium";
+      };
+
+      default = self.apps.aarch64-linux.thorium;
+    };
+  };
+}
